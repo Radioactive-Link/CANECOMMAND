50cff563ebae4ce1dc2f0f7d5f59b27623c90b4d
radioactive-link <dunsethsamiam@gmail.com>
2023-02-18 08:43:45 -0500

    Added Compressor and Solenoid to ArmSubsystem.

--- src/main/cpp/subsystems/ArmSubsystem.cpp
+++ src/main/cpp/subsystems/ArmSubsystem.cpp
@@ -1,5 +1,6 @@
 #include "subsystems/ArmSubsystem.hpp"
 
+/* --=#[ CONSTRUCTOR ]#=-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 ArmSubsystem::ArmSubsystem() 
 :
 armJoint(Constants::MotorControllers::JOINT),
@@ -22,10 +23,35 @@ armExtensionEncoder(
   Constants::Encoders::JOINT_B,
   false,
   frc::Encoder::EncodingType::k4X
-) //end of initializer list
+),
+armGrabberPiston(
+  frc::PneumaticsModuleType::CTREPCM,
+  Constants::Solenoids::ARM_PISTON
+),
+armCompressor(
+  Constants::COMPRESSOR,
+  frc::PneumaticsModuleType::CTREPCM
+)
 { //Constructor Body
   ResetEncoders();
+  StartCompressor();
 }
+/* ---===########################################===--- */
+
+
+/* --=#[ DEBUG/MANUAL CONTROL ]#=-- ~~~~~~~~~~~~~~~~~~~~ */
+void ArmSubsystem::ManualJoint() {
+  armJoint.Set(Constants::ArmConstants::Speeds::JOINT_UPWARDS);
+}
+
+void ArmSubsystem::ManualExtension() {
+
+}
+
+void ArmSubsystem::ManualGrabber() {
+
+}
+/* ---===########################################===--- */
 
 void ArmSubsystem::ResetEncoders() {
   armJointEncoder.Reset();
@@ -38,3 +64,24 @@ void ArmSubsystem::UpdateValues() {
   armGrabberDistance = armGrabberEncoder.GetDistance();
   armExtensionDistance = armExtensionEncoder.GetDistance();
 }
+
+void ArmSubsystem::ToggleGrabber() {
+  armGrabberPiston.Toggle();
+}
+
+/* --=#[ UTILITY ]#=-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+void ArmSubsystem::PrintToDashboard() {
+  UpdateValues();
+  frc::SmartDashboard::PutNumber("JOINT ENCODER: ",armJointDistance);
+  frc::SmartDashboard::PutNumber("GRABBER ENCODER: ",armGrabberDistance);
+  frc::SmartDashboard::PutNumber("EXTENSION ENCODER: ",armExtensionDistance);
+}
+
+void ArmSubsystem::StartCompressor() {
+  armCompressor.Disable();
+  armCompressor.EnableDigital();
+}
+void ArmSubsystem::StopCompressor() {
+  armCompressor.Disable();
+}
+/* ---===########################################===--- */
--- src/main/include/subsystems/ArmSubsystem.hpp
+++ src/main/include/subsystems/ArmSubsystem.hpp
@@ -3,6 +3,9 @@
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
 #include <frc/Encoder.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/Solenoid.h>
+#include <frc/Compressor.h>
 
 #include "Constants.hpp"
 #include "ctre/Phoenix.h"
@@ -11,8 +14,20 @@ class ArmSubsystem : public frc2::SubsystemBase {
 public:
   ArmSubsystem();
   ~ArmSubsystem() = default;
+
   void ResetEncoders();
   void UpdateValues();
+  void PrintToDashboard();
+  
+  void StartCompressor();
+  void StopCompressor();
+
+  void ToggleGrabber();
+
+  //= Debug/Manual Control
+  void ManualJoint();
+  void ManualExtension();
+  void ManualGrabber();
 private:
   WPI_TalonSRX armJoint;
   WPI_TalonSRX armGrabber;
@@ -20,7 +35,8 @@ private:
   frc::Encoder armJointEncoder;
   frc::Encoder armGrabberEncoder;
   frc::Encoder armExtensionEncoder;
-
+  frc::Solenoid armGrabberPiston;
+  frc::Compressor armCompressor;
   int armJointDistance;
   int armGrabberDistance;
   int armExtensionDistance;
